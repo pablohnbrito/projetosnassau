b0a8233d200edebb1049559b100115b5bedf1c23
Pablo Henrique Brito <pablo.henrique.brito@accenture.com>
2022-05-24 23:43:26 -0300

    questao 1 estrutura de dados projeto unidade 2

--- Classes Exercises/src/main.java
+++ Classes Exercises/src/main.java
@@ -1,11 +1,5 @@
-
-import Model.Manager;
-
-
 public class main {
     public static void main(String[] args) {
-        Manager gilvanelson = new Manager();
-        gilvanelson.setPassword("123");
-        gilvanelson.autenticar("321");
+
     }
 }
