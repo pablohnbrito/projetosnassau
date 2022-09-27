90603bb26293aea2b72606ece5f60d5fe0aec859
LAPTOP-S1B7761P\pablo <pablo.brito@live.com>
2022-09-26 23:11:38 -0300

    lista 1 - ok

--- Programação Avançada/lista1/ValorS.java
+++ Programação Avançada/lista1/ValorS.java
@@ -6,12 +6,12 @@ public class ValorS {
     public static void main(String[] args) {
         DecimalFormat df = new DecimalFormat("0.000000000000000");
         float S = 1f;
-        int numerador = 3, denominador = 2;
-
+        float numerador = 3, denominador = 2;
+        S += (float) 3/2;
         while (numerador <= 39) {
-            S += numerador/denominador;
             numerador += 2;
             denominador *= 2;
+            S += numerador/denominador;
         }
         System.out.println(df.format(S));
 
